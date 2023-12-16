de0bac72ddd7b38f9925257dd2335041bfdf5336
Karamtoth PawanKumar <pawankumarkaramtoth@gmail.com>
2023-12-17 04:05:49 +0530

    Java Abstraction - FrescoPlay Solution

--- JavaBeginner/src/Abstraction/Student.java
+++
@@ -1,6 +0,0 @@
-package Abstraction;
-
-abstract public class Student {
-
-    abstract public String result(String allMarks);
-}
--- JavaBeginner/src/FrescoPlayChallenges/Abstraction/Student.java
+++
@@ -1,6 +0,0 @@
-package FrescoPlayChallenges.Abstraction;
-
-abstract public class Student {
-
-    abstract public String result(String allMarks);
-}
